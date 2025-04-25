39058d0ea7930ed659a6440f9b3f87ec0d8a360d
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 19:29:20 +0330

    Add registration and login serializers for CustomUser model

--- main/serializer.py
+++ main/serializer.py
@@ -6,3 +6,28 @@ class FoodSerializer(serializers.ModelSerializer):
         model = Food
         fields = ('id', 'name', 'price', 'stringPrice' , 'quantity',
                   'ingredients', 'image', 'rating' ,'comments', 'type','created_at')
+
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('full_name', 'phone', 'password')
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    phone = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(phone=data['phone'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        data['user'] = user
+        return data
