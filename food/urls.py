8a7179715b0e4292c6bb36b1c02f58efc74acfd1
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 19:29:30 +0330

    Add import_export app and configure custom user model and JWT authentication

--- food/urls.py
+++ food/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from main.views import RegisterView, LoginView
 urlpatterns = [
 
+
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
 
