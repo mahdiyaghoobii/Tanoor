0b042f0a259f63803d3c33559fae51fece8e70b2
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 19:29:34 +0330

    Add registration and login views with serializers for user authentication

--- main/views.py
+++ main/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.status import HTTP_207_MULTI_STATUS
-from rest_framework.views import APIView
 from .models import Food
-from .serializer import FoodSerializer
+from .serializer import FoodSerializer, RegisterSerializer, LoginSerializer
 from rest_framework.request import Request
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
 
 
@@ -15,3 +13,23 @@ class FoodListView(APIView):
         foods = Food.objects.all()
         serializer = FoodSerializer(foods, many=True)
         return Response(serializer.data,  status=status.HTTP_200_OK)
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
