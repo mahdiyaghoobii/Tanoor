a8ae0f36f0adb1cc2bcf7161ce0ea5f87bc9c370
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 18:25:38 +0330

    Add Slider and Image models to the application

--- main/models.py
+++ main/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    image = models.ForeignKey('Image', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class image(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='/images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Food(models.Model):
     id = models.AutoField(primary_key=True)
