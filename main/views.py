c2301ccd54caaaaf1ab922ee48b82c1f90e99244
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 18:25:49 +0330

    Refactor FoodListView to return HTTP 200 status on successful retrieval

--- main/views.py
+++ main/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.status import HTTP_207_MULTI_STATUS
 from rest_framework.views import APIView
 from .models import Food
+from .serializer import FoodSerializer
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 # Create your views here.
 
 
 class FoodListView(APIView):
-    """
-    API view to retrieve list of foods.
-    """
-    def get(self, request, *args, **kwargs):
+    def get(self, request:Request):
         foods = Food.objects.all()
         serializer = FoodSerializer(foods, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,  status=status.HTTP_200_OK)
