9d2aab3903a2e8bcb14d8457ede3563c529ef364
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-25 19:29:26 +0330

    Add import_export app and configure custom user model and JWT authentication

--- food/settings.py
+++ food/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
