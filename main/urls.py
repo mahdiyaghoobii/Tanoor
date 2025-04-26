1755b79323cc3b69c5825e1fe4b42a05a39942f3
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-26 17:23:44 +0330

    Add basket management endpoints: add, decrease, and clear basket

--- main/urls.py
+++ main/urls.py
@@ -2,4 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('foods/', views.FoodListView.as_view(), name='foods-list'),
+    path('add-to-basket/<int:id>/', views.add_basket.as_view(), name='add_to_basket'),
+    path('decrease_basket/<int:id>/', views.decrease_basket.as_view(), name='decrease_basket'),
+    path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
+
 ]
