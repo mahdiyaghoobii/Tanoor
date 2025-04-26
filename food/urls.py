77c8b697448284d3be781493eb4a3d3fee86e54b
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-26 15:53:46 +0330

    Add Swagger UI for API documentation

--- food/urls.py
+++ food/urls.py
@@ -15,10 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from main.views import RegisterView, LoginView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="شرح مستندات API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
 
 
@@ -26,5 +42,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
