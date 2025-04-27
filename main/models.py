21ac7c3297a7c1e3bac20bf10d8c6e9d6e94b243
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-27 13:57:54 +0330

    Add status choices and created_at field to Order model for improved order tracking

--- main/models.py
+++ main/models.py
@@ -94,12 +94,20 @@ class Food(models.Model):
 
 
 class Order(models.Model):
+    status_choice = [
+        ('Pending', 'در انتظار تایید از سمت تنور'),
+        ('Accepted', 'در حال آماده سازی'),
+        ('Delivering', 'در حال ارسال'),
+        ('Completed', 'انجام شده'),
+        ('Cancelled', 'لغو شده'),
+    ]
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     food = models.ManyToManyField(Food, blank=True)
     order_date = models.DateTimeField(auto_now_add=True)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, default='Pending')
+    status = models.CharField(max_length=20, choices=status_choice, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"Order {self.id} by {self.user.full_name}"
 
