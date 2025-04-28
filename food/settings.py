2a1e463b7fdca2a9a7335f2d6ed3e11a1a3ada89
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-28 17:47:47 +0330

    Add CORS headers to settings for cross-origin requests

--- food/settings.py
+++ food/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,3 +152,6 @@ SIMPLE_JWT = {
     'AUTH_COOKIE_SECURE': True,
     'AUTH_COOKIE_SAMESITE': 'Lax',  # تنظیم SameSite برای امنیت
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
