1e2e0055c375d6c88277e92906d17eeaa0c977b4
YAKOOzA <boogeyyakooza@gmail.com>
2025-04-29 19:15:40 +0330

    Add detail view for individual food items

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('foods/', views.FoodListView.as_view(), name='foods-list'),
+    path('foods/<int:id>/', views.FoodDetailView.as_view(), name='foods-detail'),
     path('add-to-basket/<int:id>/', views.add_basket.as_view(), name='add_to_basket'),
     path('decrease_basket/<int:id>/', views.decrease_basket.as_view(), name='decrease_basket'),
     path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
