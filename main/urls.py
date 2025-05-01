4af6bd394cc9af520e2791c7e9cbf7b5d7aafdf4
YAKOOzA <boogeyyakooza@gmail.com>
2025-05-01 13:39:05 +0330

    Add order list and detail views to manage orders

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('decrease_basket/<int:id>/', views.decrease_basket.as_view(), name='decrease_basket'),
     path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
     path('submit_basket/', views.submit_basket.as_view(), name='submit_basket'),
+    path('orders/', views.OrderListView.as_view(), name='orders-list'),
+    path('orders/<int:id>/', views.OrderDetailView.as_view(), name='orders-detail'),
 
 ]
